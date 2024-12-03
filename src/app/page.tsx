abd2bc78750ba024eedbd4b17bab55deac3ecf10
glsvitoria <guivitoria2010@hotmail.com>
2024-12-03 01:33:06 -0300

    build: remove motors sale section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import CallToAction from "@/pages/Home/CallToAction";
 import Categories from "@/pages/Home/Categories";
 import Equipments from "@/pages/Home/Equipments";
 import Hero from "@/pages/Home/Hero";
-import MotorsSale from "@/pages/Home/MotorsSale";
 import Partnerships from "@/pages/Home/Partnerships";
 
 export default function Home() {
@@ -12,7 +11,7 @@ export default function Home() {
     <main className="w-[calc(100vw - 8px)] flex flex-col items-center overflow-hidden">
       <Hero />
       <Categories />
-      <MotorsSale />
+      {/* <MotorsSale /> */}
       <Equipments />
       <CallToAction />
       <AboutUs />
